1056c57f8f0f5fa9b56ec1f91e92e6a6dbc94800
Leonardo Gatica <lgatica@protonmail.com>
2016-05-15 12:47:08 -0400

    Refactor

--- src/index.js
+++ src/index.js
@@ -2,26 +2,38 @@
 
 'use strict';
 
-import program from 'commander';
-import rutStatus from 'rut-status';
-import pkg from '../package.json';
+const program = require('commander');
+const rutStatus = require('rut-status');
+const chalk = require('chalk');
+const updateNotifier = require('update-notifier');
+const pkg = require('../package.json');
+const ora = require('ora');
+
+updateNotifier({pkg}).notify();
 
 program
   .version(pkg.version)
-  .option('-r, --rut [rut]', 'Add rut', /^(\d{6,8})-[0-9k]$/i)
-  .option('-t, --type [type]', 'Add type', /^(CEDULA|CEDULA_EXT|PASAPORTE_PG|PASAPORTE_DIPLOMATICO|PASAPORTE_OFICIAL)$/i, 'CEDULA')
-  .option('-s, --serial [serial]', 'Add serial', /^a(\d{9})$/i)
+  .usage('-r <rut> -t <type> -s <serial>')
+  .description('Check rut status in registro civil')
+  .option('-r, --rut [rut]', 'Add rut. Ex: 11111111-1', /^(\d{6,8})-[0-9k]$/i)
+  .option('-t, --type [type]', 'Availables CEDULA, CEDULA_EXT, PASAPORTE_PG, PASAPORTE_DIPLOMATICO, PASAPORTE_OFICIAL', /^(CEDULA|CEDULA_EXT|PASAPORTE_PG|PASAPORTE_DIPLOMATICO|PASAPORTE_OFICIAL)$/i, 'CEDULA')
+  .option('-s, --serial [serial]', 'Add serial. Ex. A111111111', /^a(\d{9})$/i)
   .parse(process.argv);
 
 if (program.rut && program.type && program.serial) {
+  const spinner = ora('Searching...');
+  spinner.start();
   const options = {
     rut: program.rut,
     type: program.type.toUpperCase(),
     serial: program.serial
   };
-  rutStatus(options, (err, status) => {
-    if (err) console.log(err.message);
-    else console.log(`Status: ${status}`);
+  rutStatus(options).then(status => {
+    spinner.stop();
+    console.log(chalk.green(`Status: ${status}`));
+  }).catch(err => {
+    spinner.stop();
+    console.log(chalk.red(err.message));
   });
 } else {
   program.help();
